81131079015be3a2450ddef0e720c20f859c1f6e
Vitor Hugo Marcal <vitorhugomarcal@gmail.com>
2024-09-01 18:24:15 -0300

    add more controllers

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import { env } from "./env"
 
 app
   .listen({
-    host: "192.168.1.142",
+    // host: "192.168.1.142",
+    host: "0.0.0.0",
     port: env.PORT,
   })
   .then(() => {
-    console.log(`🚀 Server is running!`)
+    console.log(`🚀 HTTP Server listening on port ${env.PORT}.`)
   })
