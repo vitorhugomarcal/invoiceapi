194bb62cf09f8c1bd28302f9593d23db6893354d
Vitor Hugo Marcal <vitorhugomarcal@gmail.com>
2024-09-05 12:00:46 -0300

    change ip

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ app
   .listen({
     // host: "192.168.1.142",
     host: "0.0.0.0",
-    port: env.PORT,
+    port: env.PORT ? Number(env.PORT) : 3333,
   })
   .then(() => {
     console.log(`🚀 HTTP Server listening on port ${env.PORT}.`)
